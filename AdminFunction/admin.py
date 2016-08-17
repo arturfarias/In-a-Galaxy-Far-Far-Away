65a6b0a641d2d5e4ba6d1682f86e554e03ed662e
Artur <arcashaid@gmail.com>
2016-08-16 21:50:11 -0300

    Removendo apps sem uso

--- AdminFunction/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import CadastroLab
-
-##================= Adicionando aplicacoes ao admin django =====================
-admin.site.register(CadastroLab)
--- AdminFunction/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class CadastroLab(models.Model):
-    laboratorio = models.CharField(max_length=50,)
-
-    def __unicode__(self):
-        return self.laboratorio
