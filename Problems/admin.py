447849597a948505573218c94e82713cd204ec65
Artur <arcashaid@gmail.com>
2016-09-21 11:33:24 -0300

    Mudanças

--- Problems/admin.py
+++ Problems/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Problema, Equipamento
+from .models import Problema, Equipamento,Lab,Tipo_de_equipamento
 
+admin.site.register(Lab)
+admin.site.register(Tipo_de_equipamento)
 admin.site.register(Problema)
 admin.site.register(Equipamento)
--- Problems/models.py
+++ Problems/models.py
@@ -3,23 +3,24 @@ from django.core.exceptions import ValidationError
 from django.db.models.signals import pre_save
 from django.dispatch.dispatcher import receiver
 
+class Lab(models.Model):
+
+	laboratorio_cad = models.CharField(unique=True,max_length=6)
+
+	def __unicode__ (self):
+			return self.laboratorio_cad
+
+class Tipo_de_equipamento(models.Model):
+	tipo = models.CharField(unique=True,max_length=30)
+
+	def __unicode__ (self):
+			return self.tipo
 class Equipamento(models.Model):
 
-	laboratorio = models.CharField(
-		choices=(
-			('LECC1', 'LECC1'),
-			('LECC2', 'LECC2'),
-		), max_length = 20
-	)
-
+	laboratorio = models.ForeignKey(Lab)
 	numero_de_Serie = models.CharField(unique=True,max_length=10)
-	tipo_de_Equipamento = models.CharField(
-		choices=(
-			(u'gabinete', "Gabinete"),
-			(u'estabilizador', "Estabilizador"),
-			(u'impressora', "Impressora"),
-		), max_length = 20
-	)
+	tipo_de_Equipamento = models.ForeignKey(Tipo_de_equipamento)
+
 	class Meta:
 		ordering = ['laboratorio', 'numero_de_Serie']
 
@@ -29,7 +30,7 @@ class Equipamento(models.Model):
 class Problema(models.Model):
 
 	problema_Resolvido = models.BooleanField(default=False)
-	equipamento = models.ForeignKey(Equipamento,unique=True)
+	equipamento = models.ForeignKey(Equipamento)
 	descricao = models.TextField()
 
 	class Meta:
